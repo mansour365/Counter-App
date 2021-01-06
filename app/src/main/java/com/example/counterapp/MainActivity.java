d421fc8817a8e24814ea5f1159566584447f7ea1
Mansour <maqsoudi99@gmail.com>
2021-01-06 11:53:54 -0500

    fixed vibrate on touch + fixed status bar

--- app/src/main/java/com/example/counterapp/MainActivity.java
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,21 +41,52 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Create an onclick listener
-        resultTextView.setOnClickListener(new View.OnClickListener() {
+//        resultTextView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                //Make phone vibrate
+//                vibrateobj.vibrate(vlength);
+//
+//                //Increment the result value
+//                result++;
+//
+//                //Display the result value
+//                resultTextView.setText(result+"");
+//
+//            }
+//        });
+
+        //Experimental
+        //////////////////////////////////////////////////////////////////////////////////////////
+        //Create an onTouch Listener
+        resultTextView.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
 
-                //Make phone vibrate
-                vibrateobj.vibrate(vlength);
+                    //Make phone vibrate
+                    vibrateobj.vibrate(vlength);
 
-                //Increment the result value
-                result++;
+                    //Increment the result value
+                    result++;
 
-                //Display the result value
-                resultTextView.setText(result+"");
-
+                    //Display the result value
+                    resultTextView.setText(result+"");
+                }
+                return false;
             }
         });
+
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+
+        //If we have a saved instance state, then we'll use that (in the case when ex: orientation changes)
+        if (savedInstanceState != null) {
+            //if savedInstanceState is not null then we'll take that value and set the text to that value
+            result = savedInstanceState.getInt("result");
+            resultTextView.setText(result+"");
+        }
 
     }
 
@@ -70,34 +102,62 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         final TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
         switch(item.getItemId()){
+
+            //decrementing the counter
             case R.id.decrementItem:
+                //if already 0 show message "Already 0"
                 if(result == 0) {
                     Toast.makeText(this, "Already 0", Toast.LENGTH_SHORT).show();
                     return true;
                 }
-                else{
+                //else decrement the counter
+                else {
                     Toast.makeText(this, "Minus 1", Toast.LENGTH_SHORT).show();
                     result--;
                     resultTextView.setText(result+"");
                     return true;
                 }
+
+            //Resetting the counter
             case R.id.resetItem:
-                Toast.makeText(this, "Resetting", Toast.LENGTH_SHORT).show();
-                result = 0;
-                resultTextView.setText(result+"");
-                return true;
+                //if already 0 show message "Already 0"
+                if(result == 0){
+                    Toast.makeText(this, "Already 0", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                //else reset the counter
+                else {
+                    Toast.makeText(this, "Counter Reset", Toast.LENGTH_SHORT).show();
+                    result = 0;
+                    resultTextView.setText(result + "");
+                    return true;
+                }
+
+            //Settings page
             case R.id.settingsItem:
                 Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
                 return true;
+
+            //About page
             case R.id.aboutItem:
                 Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
                 return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
 
     }
 
+    //Save values in the app when orientation changes
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        //Values we want to save when orientation changes
+        //Result will be saved in the outside bundle
+        outState.putInt("result", result);
+    }
 
 
 }
